8afbed1489b186daa73e909e373f573b290dcba0
tkosht <takehito.oshita.business@gmail.com>
2019-06-21 03:01:28 +0900

    mod: for gpuenv:simple

--- pypj/scripts/docker/test_run.sh
+++ pypj/scripts/docker/test_run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # docker run --runtime=nvidia -it --rm gpuenv:simple /bin/bash
-# docker run --runtime=nvidia -it --rm gpuenv:simple python main.py
+docker run --runtime=nvidia -it --rm gpuenv:simple python main.py
 # docker run --runtime=nvidia -it --rm gpuenv:pyenv /bin/bash
-docker run --runtime=nvidia -it --rm gpuenv:pyenv python main.py
+# docker run --runtime=nvidia -it --rm gpuenv:pyenv python main.py
